429aa343784b50c8d797eb86414af4439f5a7a5a
Buhai Darius <buhaidarius@gmail.com>
2020-10-12 19:19:42 +0300

    Update

---
+++ Anul II/Semestrul 1/SGBD/create_table.sql
@@ -0,0 +1,10 @@
+
+Error starting at line : 47 in command -
+select 'create table DEPARTMENTS2 ' || * 
+FROM DEPARTMENTS
+Error at Command Line : 47 Column : 40
+Error report -
+SQL Error: ORA-00936: missing expression
+00936. 00000 -  "missing expression"
+*Cause:    
+*Action:
---
+++ Anul II/Semestrul 1/SGBD/sterg_tabele.sql
@@ -0,0 +1,39 @@
+
+drop table if existsEMP_TMO;
+
+drop table if existsEMP_SST;
+
+drop table if existsEMP_SOR;
+
+drop table if existsEMP_SIR;
+
+drop table if existsEMP_RTU;
+
+drop table if existsEMP_PFE;
+
+drop table if existsEMP_NTU;
+
+drop table if existsEMP_NID;
+
+drop table if existsEMP_MVC;
+
+drop table if existsEMP_LTE;
+
+drop table if existsEMP_ISA;
+
+drop table if existsEMP_CHI;
+
+drop table if existsEMP_BHD;
+
+drop table if existsEMP_ATI;
+
+drop table if existsEMP_AST;
+
+drop table if existsEMP_ARI;
+
+drop table if existsEMP_ACO;
+
+drop table if existsEMP_AAG;
+
+drop table if existsEMPLOYEES;
+
